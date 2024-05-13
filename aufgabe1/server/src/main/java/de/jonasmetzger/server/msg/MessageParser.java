90a537007be931283c6fd3118b5095f642247e84
Jonas <mail@jonasmetzger.de>
2024-05-13 13:27:32 +0200

    improve logging.

--- aufgabe1/server/src/main/java/de/jonasmetzger/server/msg/MessageParser.java
+++ aufgabe1/server/src/main/java/de/jonasmetzger/server/msg/MessageParser.java
@@ -8,48 +8,53 @@ public class MessageParser {
     final static Logger logger = LogManager.getLogger(MessageParser.class);
 
     public Response parse(String msg, String client) {
+        Response response;
+        logger.info("Incoming message from client {} with request {}", client, msg);
         if (msg.contains("\r")) return Response.err("\\r_NOT_ALLOWED");
         if (msg.startsWith("LOWERCASE")) {
             final String params = msg.substring(9).strip();
             if (params.isEmpty()) {
                 logger.info("LOWERCASE Command Received with missing Params from client {}", client);
-                return Response.err("PARAMS_MISS");
+                response = Response.err("PARAMS_MISS");
             } else {
                 logger.info("LOWERCASE Command Received from client {}", client);
-                return Response.ok(params.toLowerCase());
+                response = Response.ok(params.toLowerCase());
             }
         } else if (msg.startsWith("UPPERCASE")) {
             final String params = msg.substring(9).strip();
             if (params.isEmpty()) {
                 logger.info("UPPERCASE Command Received with missing Params from client {}", client);
-                return Response.err("PARAMS_MISS");
+                response = Response.err("PARAMS_MISS");
             } else {
                 logger.info("UPPERCASE Command Received from client {}", client);
-                return Response.ok(params.toUpperCase());
+                response = Response.ok(params.toUpperCase());
             }
         } else if (msg.startsWith("REVERSE")) {
             final String params = msg.substring(7).strip();
             if (params.isEmpty()) {
                 logger.info("REVERSE Command Received with missing Params from client {}", client);
-                return Response.err("PARAMS_MISS");
+                response = Response.err("PARAMS_MISS");
             } else {
                 logger.info("REVERSE Command Received from client {}", client);
-                return Response.ok(new StringBuilder(params).reverse().toString());
+                response = Response.ok(new StringBuilder(params).reverse().toString());
             }
         } else if (msg.startsWith("SHUTDOWN")) {
             final String params = msg.substring(8).strip();
             if (!params.equals("HAW")) {
                 logger.info("SHUTDOWN Command Received with incorrect Password from client {}", client);
-                return Response.err("INVALID_PWD");
+                response = Response.err("INVALID_PWD");
             } else {
                 logger.info("SHUTDOWN Command Received from client {}", client);
-                return Response.terminate();
+                response = Response.terminate();
             }
         } else if (msg.startsWith("BYE")) {
             logger.info("BYE Command Received from client {}", client);
-            return Response.sessionEnd();
+            response = Response.sessionEnd();
+        } else {
+            logger.info("Invalid Command Received from client {}", client);
+            response = Response.err("INVALID_CMD");
         }
-        logger.info("Invalid Command Received from client {}", client);
-        return Response.err("INVALID_CMD");
+        logger.info("Response for client {} with response '{}'", client, response.toString());
+        return response;
     }
 }
